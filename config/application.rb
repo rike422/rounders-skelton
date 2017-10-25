d426824b5f869b7a8137f0c45ea0c1cbb2d483c2
Akira Takahashi <rike422@gmail.com>
2017-10-25 12:54:59 +0900

    init

---
+++ config/application.rb
@@ -0,0 +1,4 @@
+class Rounders::Application
+  # config.logger = Logger.new('log/rounders.log')
+  # config.store = :memory
+end
---
+++ config/initializers/mail.rb
@@ -0,0 +1,16 @@
+Rounders::Receivers::Mail.configure do |config|
+  # please show more option
+  #
+  config.protocol = :imap
+  config.mail_server_setting = {
+    address:    'imap.gmail.com',
+    port:       993,
+    user_name:  ENV['USER_NAME'],
+    password:   ENV['PASSWORD'],
+    enable_ssl: true
+  }
+  config.options = {
+    # flag for whether to delete each receive mail after find Default: false
+    # delete_after_find: true
+  }
+end
